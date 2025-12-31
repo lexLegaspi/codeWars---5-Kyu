b852959023879847990c78b3355f7b30ec836fa3
Alexis Legaspi <162006706+lexLegaspi@users.noreply.github.com>
2026-01-01 00:20:31 +0900

    Update composeFunctions(Tcombinator).js

--- composeFunctions(Tcombinator).js
+++ composeFunctions(Tcombinator).js
@@ -1,16 +1,25 @@
-Let's make a function called compose that accepts a value as a parameter, as well as any number of functions as additional parameters.
+// Let's make a function called compose that accepts a value as a parameter, as well as any number of functions as additional parameters.
 
-The function will return the value that results from the first parameter being used as a parameter for all of the accepted function parameters in turn.
+// The function will return the value that results from the first parameter being used as a parameter for all of the accepted function parameters in turn.
 
-compose(n, f1, f2, f3..., fn) // === fn(...(f3(f2(f1(n)))))
-So:
+// compose(n, f1, f2, f3..., fn) // === fn(...(f3(f2(f1(n)))))
+// So:
 
-var doubleTheValue = function(val) { return val * 2; }
-var addOneToTheValue = function(val) { return val + 1; }
+// var doubleTheValue = function(val) { return val * 2; }
+// var addOneToTheValue = function(val) { return val + 1; }
 
-compose(5, doubleTheValue) // should === 10
-compose(5, doubleTheValue, addOneToTheValue) // should === 11
-If only a single parameter is passed in, return that parameter.
+// compose(5, doubleTheValue) // should === 10
+// compose(5, doubleTheValue, addOneToTheValue) // should === 11
+// If only a single parameter is passed in, return that parameter.
 
-compose(n) = n
+// compose(n) = n
 
+// My Solution:
+
+var compose = function(val, ...args) {
+    for (let i = 0; i<args.length;i++){
+      val = args[i](val)
+    }
+  
+  return val
+}
